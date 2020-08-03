514ce5f9fc6e6ea44f69495f89aec352b96c29a8
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-08-03 12:06:41 -0400

    updates

--- develop/server.js
+++ develop/server.js
@@ -1,7 +1,7 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
-var connect = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -9,44 +9,85 @@ var connect = mysql.createConnection({
     database: "employee_tracker"
 });
 
-connect.connect(function (err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connect.threadId);
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
     start();
 });
+
+function afterConnection() {
+    connection.query("SELECT * FROM employees", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  }
 
 function start() {
     inquirer
         .prompt({
             name: "option",
             type: "list",
-            message: "What would you want to do?",
-            choices: [ "View All Employees by Department", "View All Employees by Role", "View All Employees by Manager", "Add Employee", "Add Department", "Add Role", "Update Employee Role", "Update Employee Manager"]
+            message: "What would you like to do?",
+            choices: [
+            "View all Employees", 
+            "View all Roles", 
+            "View all Departments", 
+            "Add Employee", 
+            "Add Role", 
+            "Add Department", 
+            "Update Employee Role"]
         })
         .then(function (answer) {
-            if (answer.option === "View All Employees by Department") {
-                viewAllbyDept();
+            if (answer.option === "View All Employees") {
+                viewAllEmployees();
             }
-            if (answer.option === "View All Employees by Role") {
-                viewAllbyRole();
+            if (answer.option === "View All Roles") {
+                viewAllRoles();
             }
-            if (answer.option === "View All Employees by Manager") {
-                viewAllbyMgr();
+            if (answer.option === "View All Departments") {
+                viewAllDepartments();
             }
             if (answer.option === "Add Employee") {
                 addEmployee();
             }
-            if (answer.option === "Add Department") {
-                addDept();
-            }
             if (answer.option === "Add Role") {
                 addRole();
+            }
+            if (answer.option === "Add Department") {
+                addDepartment();
             }
             if (answer.option === "Update Employee Role") {
                 updateEmployeeRole();
             }
-            if (answer.option === "Update Employee Manager") {
-                updateEmployeeMgr();
-            }
         })
+};
+
+function viewAllEmployees(){
+
+};
+
+function viewAllRoles(){
+
+};
+
+function viewAllDepartments(){
+
+};
+
+function addEmployee(){
+
+};
+
+function addRole(){
+
+};
+
+function addDepartment(){
+
+};
+
+function updateEmployeeRole(){
+
 };
